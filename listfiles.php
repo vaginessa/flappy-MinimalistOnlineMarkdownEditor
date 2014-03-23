0c8a65063301769c0f4426f1ff49ace4f3808da1
vincurekf <vincurek.f@gmail.com>
2014-03-23 19:13:03 +0100

    cleaning, comments in code

--- listfiles.php
+++ listfiles.php
@@ -1,5 +1,7 @@
 <?php
 function listFolderFiles($dir,$exclude){
+    // check if we're logged in
+    // if so, add the subfolder path (same as username)
     if (isset($_COOKIE['username_log'])) {
         $loggedas = $_COOKIE['username_log'];
         $dir = $dir.'/'.$loggedas;
@@ -7,8 +9,8 @@ function listFolderFiles($dir,$exclude){
     }else{
         $ffs = scandir($dir);
     };
-
-    echo '<h3>"'.$dir.'" folder.</h3>';
+    // show file list
+    echo '<strong>"'.$dir.'" folder.</strong>';
     echo '<ul class="list_files">';
     foreach($ffs as $ff){
         if(is_array($exclude) and !in_array($ff,$exclude)){
@@ -16,21 +18,19 @@ function listFolderFiles($dir,$exclude){
             if(!is_dir($dir.'/'.$ff)){
                 $info = pathinfo($ff);
                 $file_name =  basename($ff,'.'.$info['extension']);
-                // check for privileges
                 echo '<li>';
-                    if (isset($loggedas)) {
-                        echo '<a href="#" onClick=\'deleteFile("'.$file_name.'", "'.$loggedas.'")\'>';
-                        echo '<svg version="1.1" id="delete_icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                // check for privileges, if logged show the delete button
+                if (isset($loggedas)) {
+                echo '<a href="#" onClick=\'deleteFile("'.$file_name.'", "'.$loggedas.'")\'>';
+                echo '<svg version="1.1" id="delete_icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      width="12px" height="12px" viewBox="0 0 20 20" style="enable-background:new 0 0 20 20;" xml:space="preserve">
-<polygon class="delete_icon_cross" points="17.778,4.343 15.657,2.222 10,7.879 4.343,2.222 2.222,4.343 7.879,10 2.222,15.657 4.343,17.778 10,12.121 
-    15.657,17.778 17.778,15.657 12.121,10 "/>
-</svg>';
-                        echo '</a> - ';
-                    };
+    <polygon class="delete_icon_cross" points="17.778,4.343 15.657,2.222 10,7.879 4.343,2.222 2.222,4.343 7.879,10 2.222,15.657 4.343,17.778 10,12.121 
+    15.657,17.778 17.778,15.657 12.121,10 "/></svg></a> - ';
+                };
                 echo '<a href="#" onClick=\'loadFromFile("'.$file_name.'")\'>'.$ff.'</a>'; 
-            } else {
-                echo '<li>'.$ff;   
-            }
+                } else {
+                    echo '<li>'.$ff.'</li>';   
+                }
                 //if(is_dir($dir.'/'.$ff)) listFolderFiles($dir.'/'.$ff,$exclude);
                 //echo '</li>';
             }
